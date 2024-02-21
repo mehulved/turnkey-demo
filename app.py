cdcd655f375425a194801e51c6ef9e24caea5672
Mehul Ved <mehul.n.ved@gmail.com>
2024-02-21 23:10:23 +0530

    Ability to run dev and multiple turnkey apps in parallel.

--- app.py
+++ app.py
@@ -5,12 +5,18 @@ import aws_cdk as cdk
 
 from turnkey.turnkey_stack import TurnkeyStack
 
-
 app = cdk.App()
-TurnkeyStack(app, "TurnkeyStack",
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+branch = app.node.try_get_context("branch")
+if branch:
+    TurnkeyStack(app, f"TurnkeyStack-{branch}",
+                 env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+                 # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+                 )
+else:
+    TurnkeyStack(app, f"TurnkeyStack-dev",
+                 env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
+                 # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+                 )
 app.synth()
